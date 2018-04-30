403c9e7a0c8fbe1e73a4ee3befb13a899c06465c
clandestina-hds <patriciagonzalezgaspar@gmail.com>
2018-04-29 19:29:52 -0500

    Versión 1.0 de herramienta pyTral

---
+++ Interfaz.py
@@ -0,0 +1,224 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 28 21:36:43 2017
+
+@author: Clandestina
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  5 09:52:17 2016
+
+@author: Clandestina
+"""
+
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import (QApplication, QPushButton, QSplitter, QFileDialog, QMessageBox, QLabel, QComboBox,QWidget, QCheckBox)
+from PyQt5.QtCore import Qt
+import pyqtgraph as pg 
+from Operations import Operations
+import numpy as np
+import pandas as pd
+
+#%%
+class fractalSignal(QWidget):
+    def __init__(self):
+        super(fractalSignal,self).__init__()
+        self.initUI()
+
+#%% 
+    def initUI(self):
+        pg.setConfigOption('background', 'w')
+        self.fullSignal=[]
+        self.shiftFullSignal=[]
+        self.shiftFullSignalNormal=[]
+        
+        self.fSig=''
+        
+        contenedor=QtWidgets.QHBoxLayout(self)
+        
+        contain=QSplitter(Qt.Horizontal)
+
+        buttons = QtWidgets.QVBoxLayout()
+        graphics = QtWidgets.QVBoxLayout()
+        imaFrac = QtWidgets.QHBoxLayout()
+       
+        self.btnLoadSig = QPushButton('Load Signal')
+        self.btnLoadSig.clicked.connect(self.loadSignal)
+        self.btnLoadSig.setStyleSheet("background-color:#fbe9e7")
+        
+        self.lblSignal = QLabel('')
+        
+        self.checkTotalSignal = QCheckBox('Signal', self)
+        
+
+        self.cmbFractal = QComboBox()
+        self.cmbFractal.setStyleSheet("background-color:#fbe9e7")
+        self.cmbFractal.addItem("Triangle") #Elemento 0
+        self.cmbFractal.addItem("Square") #Elemento 1
+        self.cmbFractal.addItem("Pentagon") #Elemento 2
+        self.cmbFractal.addItem("Hexagon") #Elemento 3
+        #self.cmbFractal.addItem("Octgon") #Ahora éste es el elemento 4
+        
+        self.btnDo = QPushButton("Do Fractal")
+        self.btnDo.setDisabled(True)
+        self.btnDo.clicked.connect(self.showDialog)
+
+        self.btnFracInter = QPushButton("Points-Inter")
+        self.btnFracInter.setDisabled(True)
+        self.btnFracInter.clicked.connect(self.update)
+        
+        self.btnSave = QPushButton("Save Current Data")
+        self.btnSave.setDisabled(True)
+        self.btnSave.clicked.connect(self.saveFile)
+        
+        self.viewBox=pg.GraphicsLayoutWidget()
+        self.interFrac = self.viewBox.addViewBox(row=0, col=0, lockAspect=True)
+        self.poinc = self.viewBox.addViewBox(row=0, col=1, lockAspect=True)
+        
+        self.scaInter=pg.ScatterPlotItem()
+        self.scaPoinc=pg.ScatterPlotItem()
+
+        self.roiInter=pg.PolyLineROI([[0.2, 0.5], [0.8, 0.5], [0.5, 0]], pen=(6,9), closed=True)
+
+        imaFrac.addWidget(self.viewBox)
+
+        buttons.setSizeConstraint(0)
+        
+        buttons.addWidget(self.btnLoadSig)
+        buttons.addWidget(self.lblSignal)
+        
+        buttons.addWidget(self.checkTotalSignal)
+        
+        buttons.addWidget(QLabel("Fractal Type"))
+        buttons.addWidget(self.cmbFractal)
+        
+        buttons.addWidget(self.btnDo)
+        buttons.addWidget(self.btnFracInter)
+        buttons.addWidget(self.btnSave)
+        
+        self.plot1=pg.PlotWidget()
+        graphics.addLayout(imaFrac)
+        graphics.addWidget(self.plot1)
+        bot = QWidget()
+        bot.setLayout(buttons)
+        gra = QWidget()
+        gra.setLayout(graphics)
+        
+        
+        contain.addWidget(bot)
+        contain.addWidget(gra)
+        contenedor.addWidget(contain)
+        self.setLayout(contenedor)
+
+        self.showMaximized()
+        self.show()
+
+        
+#%%
+    def loadSignal(self):
+        self.fSig = QFileDialog.getOpenFileName(None, 'Open file', '/home')
+        if(self.fSig[0] !=''):
+            self.btnLoadSig.setStyleSheet("background-color:#e8f5e9")
+            names=str.split(self.fSig[0],"/")
+            t=len(names)
+            self.lblSignal.setText(names[t-1])
+            self.btnDo.setEnabled(True)
+
+#%%
+    def enableFields(self):
+        self.lblGrade.setEnabled(True)
+        self.txtN.setEnabled(True)
+        self.lblFcL.setEnabled(True)
+        self.txtFcL.setEnabled(True)
+        self.lblFcH.setEnabled(True)
+        self.txtFcH.setEnabled(True)
+
+#%%     
+    def showDialog(self):
+        if(self.fSig[0] !=''):
+            self.btnDo.setStyleSheet("background-color:#e8f5e9")
+            self.fullSignal = pd.read_csv(self.fSig[0], header=None)
+            self.fullSignal = self.fullSignal.as_matrix()
+            self.shiftFullSignal = Operations.shiftSignal(self.fullSignal)
+            Operations.noiseDelete(self.shiftFullSignal)
+            
+            tam = len(self.shiftFullSignal)
+            
+            self.shiftFullSignal = np.asarray(self.shiftFullSignal).reshape(tam,)
+            self.shiftFullSignalNormal= Operations.normalSignal(self.shiftFullSignal)
+            self.shiftFullSignalNormal = np.asarray(self.shiftFullSignal).reshape(tam,)
+
+            if(self.cmbFractal.currentIndex()==0):
+                self.fracta= Operations.fracTriangle(self.shiftFullSignalNormal)
+            elif(self.cmbFractal.currentIndex()==1):
+                self.fracta = Operations.fracSquare(self.shiftFullSignalNormal)
+            elif(self.cmbFractal.currentIndex()==2):
+                self.fracta = Operations.fracPenta(self.shiftFullSignalNormal)
+            elif(self.cmbFractal.currentIndex()==3):
+                self.fracta = Operations.fracHexa(self.shiftFullSignalNormal)
+#            elif(self.cmbFractal.currentIndex()==4): #El elemento 4
+#                self.fracta = Operations.fracOcta(self.shiftFullSignalNormal) #Llamar a la función que acabamos de crear
+                
+            x, y = Operations.graphPoinc(self.shiftFullSignalNormal)
+                
+            self.scaInter.setData(self.fracta[:,3], self.fracta[:,4], pen=None, symbolPen='r', symbolBrush='r', symbol='o')
+            self.scaInter.setSize(2)
+            
+            self.interFrac.addItem(self.scaInter)
+            self.interFrac.addItem(self.roiInter)
+            
+            self.scaPoinc.setData(x, y, pen=None, symbolPen='r', symbolBrush='r', symbol='o')
+            self.scaPoinc.setSize(2)
+            self.poinc.addItem(self.scaPoinc)
+           
+            self.timeVector = np.asarray(range(tam)).reshape(tam,)
+            self.plot1.plot(self.timeVector,self.shiftFullSignalNormal,pen='k')
+            
+            self.btnFracInter.setEnabled(True)
+   
+        else:
+            QMessageBox.critical(None, "Alerta","You must select a file and the channels corresponding to signal and integrated signal", QMessageBox.Ok)
+            
+#%%        
+    def update(self):
+        self.plot1.clear()
+        self.plot1.plot(self.timeVector,self.shiftFullSignalNormal,pen='k')
+        roiShape = self.roiInter.mapToItem(self.scaInter, self.roiInter.shape())
+        selected = [pt for pt in self.scaInter.points() if roiShape.contains(pt.pos())]
+        print(len(selected))
+        self.inde=self.pointsInRoi(self.fracta[:,3], self.fracta[:,4],selected)
+        self.plot1.plot(self.inde, self.shiftFullSignalNormal[self.inde], pen=None, symbol='o', symbolPen='m', symbolBrush='m', symbolSize=6)
+        self.btnSave.setEnabled(True)
+        
+#%%
+    def pointsInRoi(self, fracta1, fracta2, sel):
+        inde=[]
+        tam=len(fracta1)
+        for pt in sel:
+            x=pt.pos()[0]
+            y=pt.pos()[1]
+            for i in range(tam):
+                if((x==fracta1[i])&(y==fracta2[i])):
+                    inde.append(i)
+        inde=np.asarray(inde)
+        return inde
+    
+#%%
+    def saveFile(self):
+        nom = QFileDialog.getSaveFileName(None, 'Saving Data in Current Plot','', '*.csv')
+        tam=len(self.inde)
+        arr=np.zeros((tam,2))
+        arr[:,0]=self.inde
+        arr[:,1]=self.shiftFullSignalNormal[self.inde]
+        df =pd.DataFrame(arr)
+        df.to_csv(nom[0], index=False, mode='w', header=False)
+        
+
+#%%     
+#
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    mw=fractalSignal()
+    sys.exit(app.exec_())
---
+++ Operations.py
@@ -0,0 +1,323 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 20 00:59:26 2017
+
+@author: Clandestina
+"""
+
+import numpy as np
+
+class Operations():
+#%%
+    def fracTriangle(senial):
+        tam = len(senial) 
+        senial=senial.reshape((tam,))
+        fracta = np.zeros((tam, 5))
+        fracta[:,0] = np.arange(tam)
+        fracta[:,1] = senial
+        fracta = fracta[fracta[:,1].argsort()[::-1]]
+        tamGroup=tam//3
+        ini=0
+        fin=tamGroup
+        for i in range(3):
+            fracta[ini:fin,2]=i+1
+            ini=ini+tamGroup
+            fin=fin+tamGroup
+        fracta = fracta[fracta[:,0].argsort()]
+        pInicial=[0.5,0.33]
+        p1=[0,0]
+        p2=[1,0]
+        p3=[0.5,1]
+        for i in range(tam):
+            if(fracta[i,2]==1):
+                pInicial=np.mean([p1,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==2):
+                pInicial=np.mean([p2,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==3):
+                pInicial=np.mean([p3,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+        #fracta=np.round(fracta,6)
+        return fracta
+    
+#%%
+    def fracSquare(senial):
+        tam = len(senial) 
+        senial=senial.reshape((tam,))
+        fracta = np.zeros((tam, 5))
+        fracta[:,0] = np.arange(tam)
+        fracta[:,1]=senial
+        
+        fracta = fracta[fracta[:,1].argsort()[::-1]]
+        tamGroup=tam//4
+        ini=0
+        fin=tamGroup
+        for i in range(4):
+            fracta[ini:fin,2]=i+1
+            ini=ini+tamGroup
+            fin=fin+tamGroup
+        fracta = fracta[fracta[:,0].argsort()]
+        pInicial=[0.5,0.33]
+        p1=[0,0]
+        p2=[1,0]
+        p4=[1,1]
+        p3=[0,1]
+        for i in range(tam):
+            if(fracta[i,2]==1):
+                pInicial=np.mean([p1,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==2):
+                pInicial=np.mean([p2,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==3):
+                pInicial=np.mean([p3,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==4):
+                pInicial=np.mean([p4,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+        #fracta=np.round(fracta,6)
+        return fracta
+ 
+#%%
+    def fracPenta(senial):
+        tam = len(senial) 
+        senial=senial.reshape((tam,))
+        fracta = np.zeros((tam, 5))
+        fracta[:,0] = np.arange(tam)
+        fracta[:,1]=senial
+        
+        fracta = fracta[fracta[:,1].argsort()[::-1]]
+        tamGroup=tam//5
+        ini=0
+        fin=tamGroup
+        for i in range(5):
+            fracta[ini:fin,2]=i+1
+            ini=ini+tamGroup
+            fin=fin+tamGroup
+        fracta = fracta[fracta[:,0].argsort()]
+        pInicial=[0.5,0.4]
+        p1=[0.25,0]
+        p2=[0.75,0]
+        p3=[1,0.5]
+        p4=[0,0.5]
+        p5=[0.5,1]
+        for i in range(tam):
+            if(fracta[i,2]==1):
+                pInicial=np.mean([p1,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==2):
+                pInicial=np.mean([p2,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==3):
+                pInicial=np.mean([p3,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==4):
+                pInicial=np.mean([p4,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==5):
+                pInicial=np.mean([p5,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+        #fracta=np.round(fracta,6)
+        return fracta
+    
+#%%
+    def fracHexa(senial):
+        tam = len(senial) 
+        senial=senial.reshape((tam,))
+        fracta = np.zeros((tam, 5))
+        fracta[:,0] = np.arange(tam)
+        fracta[:,1]=senial
+
+        fracta = fracta[fracta[:,1].argsort()[::-1]]
+        tamGroup=tam//6
+        ini=0
+        fin=tamGroup
+        for i in range(6):
+            fracta[ini:fin,2]=i+1
+            ini=ini+tamGroup
+            fin=fin+tamGroup
+        fracta = fracta[fracta[:,0].argsort()]
+        pInicial=[0.5,0.5]
+        p1=[0.25,0]
+        p2=[0.75,0]
+        p3=[1,0.5]
+        p4=[0.75,1]
+        p5=[0.25,1]
+        p6=[0,0.5]
+        for i in range(tam):
+            if(fracta[i,2]==1):
+                pInicial=np.mean([p1,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==2):
+                pInicial=np.mean([p2,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==3):
+                pInicial=np.mean([p3,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==4):
+                pInicial=np.mean([p4,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==5):
+                pInicial=np.mean([p5,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+            elif(fracta[i,2]==6):
+                pInicial=np.mean([p6,pInicial],axis=0)
+                fracta[i,3]=pInicial[0]
+                fracta[i,4]=pInicial[1]
+        #fracta=np.round(fracta,6)
+        return fracta
+		
+#%%
+#    def nombredefractal(senial):
+#        tam = len(senial) 
+#        senial=senial.reshape((tam,))
+#        fracta = np.zeros((tam, 5))
+#        fracta[:,0] = np.arange(tam)
+#        fracta[:,1]=senial
+#        N=Número de vértices
+#
+#        fracta = fracta[fracta[:,1].argsort()[::-1]]
+#        tamGroup=tam//N 
+#        ini=0
+#        fin=tamGroup
+#        for i in range(N): 
+#            fracta[ini:fin,2]=i+1
+#            ini=ini+tamGroup
+#            fin=fin+tamGroup
+#        fracta = fracta[fracta[:,0].argsort()]
+#        pInicial=[0.5,0.5] #Este es el punto inicial
+#        #Estas son las coordenadas de los vértices, agregar hasta pN
+#        p1=[0.25,0]
+#        p2=[0.75,0]
+#        p3=[1,0.5]
+#        p4=[0.75,1]
+#        p5=[0.25,1]
+#        p6=[0,0.5]
+#        for i in range(tam):
+#            if(fracta[i,2]==1): #Aqui se pregunta por la clase a la que pertenece
+#                pInicial=np.mean([p1,pInicial],axis=0)#Coincide con el punto 1 
+#                fracta[i,3]=pInicial[0]
+#                fracta[i,4]=pInicial[1]
+#            elif(fracta[i,2]==2):
+#                pInicial=np.mean([p2,pInicial],axis=0)
+#                fracta[i,3]=pInicial[0]
+#                fracta[i,4]=pInicial[1]
+#            elif(fracta[i,2]==3):
+#                pInicial=np.mean([p3,pInicial],axis=0)
+#                fracta[i,3]=pInicial[0]
+#                fracta[i,4]=pInicial[1]
+#            elif(fracta[i,2]==4):
+#                pInicial=np.mean([p4,pInicial],axis=0)
+#                fracta[i,3]=pInicial[0]
+#                fracta[i,4]=pInicial[1]
+#            elif(fracta[i,2]==5):
+#                pInicial=np.mean([p5,pInicial],axis=0)
+#                fracta[i,3]=pInicial[0]
+#                fracta[i,4]=pInicial[1]
+#            #Agregar un bloque como las siguientes 4 líneas para cada clase extra 
+#            #En la primer línea cambiar por el punto que se está examinando 
+#            #al igual que la clase
+#            #(cuidar la identación)
+#            elif(fracta[i,2]==6):
+#                pInicial=np.mean([p6,pInicial],axis=0)
+#                fracta[i,3]=pInicial[0]
+#                fracta[i,4]=pInicial[1]
+#        #fracta=np.round(fracta,6)
+#        return fracta
+       
+#%%
+    def noiseDelete(signa):
+        M1 = np.max(signa)
+        pos = np.argmax(signa)
+        signa2=np.delete(signa,pos)
+        M2 = np.max(signa2)
+        if(M1-M2>0.5):
+            print("APLICANDO DESRUIDIZACION")
+            positivos = signa[signa>0]
+            pos2 = np.argmax(positivos)
+            if (pos2>10):
+                sLeft = positivos[pos2-10:pos2]
+                sRight = positivos[pos2+1:pos2+10]
+                values = np.append(sLeft,sRight)
+                m = np.mean(values)
+                print("VF: ",m)
+                signa[pos]=m
+        else:
+            print("No se aplica eliminación de ruido")
+        
+#%%    
+    def normalSignal(signa):
+        Ma = np.max(signa)
+        res = signa/Ma
+        res = np.squeeze(res)
+        n=len(res)
+        res=res.reshape(n,1)
+        return res
+    
+#%%    
+    #signal : array
+    def shiftSignal(signa):
+        M=np.mean(signa)
+        res = signa-M
+        res = np.squeeze(res)
+        n=len(res)
+        res=res.reshape(n,1)
+        return res
+    
+#%%
+    def graphPoinc(signa):
+        tam = len(signa)
+        x = signa[0:tam-1]
+        y = signa[1:tam]
+        return x, y
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
